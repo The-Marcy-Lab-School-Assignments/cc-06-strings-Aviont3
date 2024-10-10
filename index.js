c10b8ad51fb6070fa73b106b9ab9ea239886dba8
Avionte Williams <aviontewilliams@gmail.com>
2024-10-10 11:03:31 -0400

    func 1 only

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+const reverseString = (str) => {
+  let reverseStr=""
+    for(let i = str.length -1 ; i >= 0;i--){
+        reverseStr += str[i]
+    }
+    return reverseStr
+};
+reverseZigZagString= () => {
+    
+    let reverseStr=""
+    for(let i = str.length -1 ; i >= 0;i--){
+        reverseStr += str[i]
+        if 
+}
